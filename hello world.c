797bc4b1a5ae5978f92c0ac74b6cb063a3722d99
manoj0510 <52616654+manoj0510@users.noreply.github.com>
2019-07-08 21:18:58 +0530

    Create hello world.c

---
+++ hello world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int n;
+    printf("enter no of times");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("hello world \n");
+    }
+    
+    
+    getch();
+}
